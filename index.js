6ad7fb93c272c75ae7120607164a92531c3d37fb
Gavin Veitch <gavintveitch@gmail.com>
2022-09-22 10:07:35 -0600

    commit tree ad1cf62fc2cfc073bde4391a5b8d1cc9bb632e48

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function (list) {
+    return list.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (list) {
+    return list.slice(2, 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const fareSame = (cost) => cost;
+
+const fareDoubler = (cost) => '2' * cost;
+
+const fareTripler = (cost) => '3' * cost;
+
+const fareQuadrupler = (cost) => '4' * cost;
+
+const fareQuintupler = (cost) => '5' * cost;
+
+
+function createFareMultiplier(multi) {
+
+    if(multi = 1){
+        return fareSame;
+    }
+
+    if(multi = 2){
+        return fareDoubler;
+    }
+
+    if(multi = 3){
+        return fareTripler;
+    }
+
+    if(multi = 4){
+        return fareQuadrupler;
+    }
+
+    if(multi = 5){
+        return fareQuintupler;
+    }
+}
+
+function selectDifferentDrivers(list, func) {
+    if(func === returnFirstTwoDrivers) {
+        return list.slice(0, 2);
+    }
+
+    if(func === returnLastTwoDrivers) {
+        return list.slice(2)
+    }
+}
